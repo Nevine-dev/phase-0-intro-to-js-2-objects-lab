3fe31d9d66d660f2302b4b712f025a8d2d3b391d
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-03-15 15:58:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Dyes Junior',
+    streetAddress: 'Juja, Kiambu',
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    return {...employee, [name]: streetAddress}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name] = streetAddress
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = Object.assign({}, employee)
+    delete newEmployee[name]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee[name]
+    return employee
+}
